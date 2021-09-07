5219c5fa20bca265ba34b8d231347f53ec41a143
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-09-06 22:34:05 -0700

    spaces

--- Leetcode/404. Sum of Left Leaves/solution.py
+++ Leetcode/404. Sum of Left Leaves/solution.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         def dfs(root, left):
