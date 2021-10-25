4f1d8f1a39dd05ffe08c3b52c57e976bb317aac2
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-10-25 07:05:33 -0700

    1360

---
+++ Leetcode/1360. Number of Days Between Two Dates/readme.md
@@ -0,0 +1,33 @@
+# Algorithm Idea
+
+Count the number of days from 1970 for each date and get the abs of the substraction. As improvement you can add cache to speed up.
+
+# Complexity
+
+Approach 1)
+
+- Time: O(n)
+
+- Space:O(1)
+
+Approach 2) Using cache to speed up.
+
+- Time: O(n)
+
+- Space:O(n)
+
+# Results
+
+Python)
+
+- Runtime: 32 ms, faster than 75.77% of Python3 online submissions for Number of Days Between Two Dates.
+
+- Memory Usage: 14.2 MB, less than 69.44% of Python3 online submissions for Number of Days Between Two Dates.
+
+Approach 2)
+
+Python)
+
+- Runtime: 24 ms, faster than 96.93% of Python3 online submissions for Number of Days Between Two Dates.
+
+- Memory Usage: 14.3 MB, less than 39.24% of Python3 online submissions for Number of Days Between Two Dates.
---
+++ Leetcode/1360. Number of Days Between Two Dates/solution2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        year_mem = {}
+
+        def is_leap_year(year: int):
+            return True if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0) else False
+
+        def count_days_date(date: str):
+            year, month, day = map(int, date.split("-"))
+            is_leap = is_leap_year(year)
+            days: int = 0
+
+            # add days per year
+            for y in range(1970, year):
+                if y in year_mem:
+                    days += year_mem[y]
+                    continue
+                days_to_add = 366 if is_leap_year(y) else 365
+                year_mem[y] = days_to_add
+                days += days_to_add
+
+            # add days per month
+            for m in range(1, month):
+                if m == 2:
+                    days += 29 if is_leap else 28
+                    continue
+
+                if m in [1, 3, 5, 7, 8, 10]:
+                    days += 31
+                else:
+                    days += 30
+
+            days += day
+
+            return days
+
+        return abs(count_days_date(date1) - count_days_date(date2))
