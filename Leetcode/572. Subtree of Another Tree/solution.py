fcbadee38c6a321d9859337118d13ee58b980504
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-11-14 20:20:25 -0800

    adding space

--- Leetcode/572. Subtree of Another Tree/solution.py
+++ Leetcode/572. Subtree of Another Tree/solution.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def is_equal(root, subRoot):
