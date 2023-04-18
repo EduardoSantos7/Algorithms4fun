f000715516cfb117f8357338f24a8973495796eb
Eduardo Santos Delgado <eduardosa@microsoft.com>
2023-04-17 20:23:11 -0700

    405

---
+++ Leetcode/405. Convert a Number to Hexadecimal/readme.md
@@ -0,0 +1,12 @@
+# Algorithm Idea
+
+8 where each char means 4 bytes -> 32 bits, iterate and resolve for every character
+
+# Complexity
+
+- Time: O(1)
+
+- Space:O(1)
+
+# Results
+
---
+++ Leetcode/405. Convert a Number to Hexadecimal/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def toHex(self, num):
+
+        if num == 0:
+            return '0'
+
+        mp = '0123456789abcdef'
+        ans = ''
+
+        for i in range(8):
+            n = num & 15
+            c = mp[n]
+            ans = c + ans
+            num = num >> 4
+
+        return ans.lstrip('0')
