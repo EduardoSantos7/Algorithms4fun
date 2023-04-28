47bf89f060d9fbf50fffb8443a800361978b714c
Eduardo Santos Delgado <eduardosa@microsoft.com>
2023-04-27 23:44:21 -0700

    657

---
+++ Leetcode/657. Robot Return to Origin/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for m in moves:
+            if m == "U":
+                y += 1
+            elif m == "D":
+                y -= 1
+            elif m == "R":
+                x += 1
+            else:
+                x -= 1
+        
+        return x == 0 and y == 0
