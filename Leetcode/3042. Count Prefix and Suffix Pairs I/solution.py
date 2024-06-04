d704f8b238efbba283c58d58751cc0ea725e4863
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-06-04 08:25:54 -0700

    3042

---
+++ Leetcode/3042. Count Prefix and Suffix Pairs I/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPrefixAndSuffix(self, w1, w2):
+        return True if w2.startswith(w1) and w2.endswith(w1) else False 
+
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        ans = 0
+        for i in range(len(words) - 1):
+            for j in range(i + 1, len(words)):
+                if self.isPrefixAndSuffix(words[i], words[j]):
+                    ans += 1
+        return ans
