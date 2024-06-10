5de6f6e939c0cbf4d105c8f6f4aeff0e33986901
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-06-10 11:23:33 -0700

    2255

---
+++ Leetcode/2255. Count Prefixes of a Given String/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        ans = 0
+        for word in words:
+            if s.startswith(word):
+                ans += 1
+        return ans
