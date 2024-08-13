85d02d6ca2748a6466efc2bdcbba6f895636bf22
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-08-13 16:07:02 -0700

    1812

---
+++ Leetcode/1812. Determine Color of a Chessboard Square/readme.md
@@ -0,0 +1,24 @@
+# Algorithm Idea
+
+If the column starts black then if the number is even then the square is white else black
+If the column starts white then even numbers are black and odds white.
+
+# Complexity
+
+- Time: O(1)
+
+- Space:O(1)
+
+# Results
+
+Runtime
+0
+ms
+Beats
+100.00%
+Analyze Complexity
+Memory
+7.54
+MB
+Beats
+7.08%
---
+++ Leetcode/1812. Determine Color of a Chessboard Square/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        std::map<char, bool> flags = {
+            {'a', false}, {'b', true}, {'c', false}, {'d', true},
+            {'e', false}, {'f', true}, {'g', false}, {'h', true}
+        };
+        bool even = ((coordinates[1] - '0') % 2) == 0;
+        if (flags[coordinates[0]] && even) {
+            return false;
+        }
+        else if (flags[coordinates[0]] && !even) {
+            return true;
+        }
+        else if (!flags[coordinates[0]] && even) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
