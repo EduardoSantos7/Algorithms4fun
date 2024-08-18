d17b09eca5ac7ff4d0e9f48a18bdc1f44a59f1a5
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-08-18 12:10:05 -0700

    1758

---
+++ Leetcode/1758. Minimum Changes To Make Alternating Binary String/readme.md
@@ -0,0 +1,23 @@
+# Algorithm Idea
+
+Iterate the string like if you were starting from zero and one and count which produce the minimum number of changes.
+
+# Complexity
+
+- Time: O(N)
+
+- Space:O(1)
+
+# Results
+
+Runtime
+3
+ms
+Beats
+80.90%
+
+Memory
+8.48
+MB
+Beats
+48.14%
---
+++ Leetcode/1758. Minimum Changes To Make Alternating Binary String/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int start0 = 0; const int n = s.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            if(i % 2 == 0) 
+            {
+                if (s[i] == '1')
+                {
+                    start0++;
+                }
+            }
+            else
+            {
+                if (s[i] == '0')
+                {
+                    start0++;
+                }
+            }
+            
+        }
+
+        return min(start0, (int) n - start0);
+    }
+};
