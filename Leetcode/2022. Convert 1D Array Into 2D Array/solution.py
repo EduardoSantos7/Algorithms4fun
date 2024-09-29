c8013984c20e9e065d785fb9eded95c0b57e1dc6
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-09-29 12:23:08 -0700

    2022

---
+++ Leetcode/2022. Convert 1D Array Into 2D Array/solution.py
@@ -0,0 +1,3 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        return [original[i:i+n] for i in range(0, m*n, n)] if m*n == len(original) else []
