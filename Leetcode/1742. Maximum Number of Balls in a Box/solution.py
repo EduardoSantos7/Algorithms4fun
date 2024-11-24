21d72ff0e109f951d2997a97c5ad3c66071a3000
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-11-23 21:20:31 -0800

    1742

---
+++ Leetcode/1742. Maximum Number of Balls in a Box/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        boxes = {}
+        for ball in range(lowLimit, highLimit + 1):
+            value = 0
+            while ball > 0:
+                value += ball % 10
+                ball //= 10
+
+            boxes[value] = 1 if value not in boxes else boxes[value] + 1
+
+        return max(boxes.values()) 
