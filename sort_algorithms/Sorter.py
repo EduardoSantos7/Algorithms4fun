79a74b8d869ec48fc08d73616b97a577063eacef
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-12-15 20:54:32 -0800

    base of sort algorithms

---
+++ sort_algorithms/Sorter.py
@@ -0,0 +1,106 @@
+from abc import ABC, abstractmethod
+
+class Sorter(ABC):
+    @abstractmethod
+    def sort(self, enumerable):
+        pass
+
+class InPlaceMergeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Heapsort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Introsort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class MergeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class TournamentSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class TreeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class BlockSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Smoothsort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Timsort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class PatienceSorting(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Cubesort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Quicksort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class LibrarySort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class Shellsort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class CombSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class InsertionSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class BubbleSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class CocktailShakerSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class GnomeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class OddEvenSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class SimplePancakeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class StrandSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class SelectionSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class ExchangeSort(Sorter):
+    def sort(self, enumerable):
+        pass
+
+class CycleSort(Sorter):
+    def sort(self, enumerable):
+        pass
