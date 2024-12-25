b7a0e47d259c5b9cda7a93a266c3b84af62b66c1
Eduardo Luis Santos <eduardosa@microsoft.com>
2024-12-24 20:54:38 -0800

    3046

--- Leetcode/3046. Split the Array/solution.py
+++ Leetcode/3046. Split the Array/solution.py
@@ -1,4 +1,5 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
         frequencies = Counter(nums)
-        return not any([val > 2 for val in frequencies.values()])
+        return not any(
+            [val > 2 for val in frequencies.values()])
