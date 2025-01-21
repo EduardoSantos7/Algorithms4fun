f1c961595403dd7e313692370b7965723670a68b
Eduardo Luis Santos <eduardosa@microsoft.com>
2025-01-20 16:54:10 -0800

    986

--- Leetcode/986. Interval List Intersections/solution.py
+++ Leetcode/986. Interval List Intersections/solution.py
@@ -1,31 +1,18 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        # create an array of seize of the biggest element in both arrays init in zeros
-        _max = max(firstList[-1][1] if firstList else 0, secondList[-1][1] if secondList else 0)
-        time = [0 for _ in range(_max + 1)]
-        print(time)
-        events = firstList + secondList
-        # Traverse both list adding 1 in the array per every time slot in the range
-        for event in events:
-            start, end = event
-            for i in range(start, end + 1):
-                time[i] += 1
-        # for every event check if there where intersections by checking where the sum is bigger than 1
         intersections = []
-        print(time)
-        for event in firstList:
-            start, end = event
-            intersection = []
-            for i in range(start, end + 1):
-                if not intersection and time[i] > 1:
-                    intersection.append(i)
-                elif intersection and time[i] == 1:
-                    intersection.append(i-1)
-                    intersections.append(intersection)
-                    intersection = []
-            if intersection:
-                intersection.append(end)
-                intersections.append(intersection)
+        i = j = 0
 
+        while i < len(firstList) and j < len(secondList):
+            lo = max(firstList[i][0], secondList[j][0])
+            hi = min(firstList[i][1], secondList[j][1])
+            if lo <= hi:
+                intersections.append([lo, hi])
+            
+            if firstList[i][1] < secondList[j][1]:
+                i += 1
+            else:
+                j += 1
+        
         return intersections
         
