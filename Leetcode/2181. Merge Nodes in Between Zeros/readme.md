76ea1f055c0db81af30b5ab71fc683f8119ee166
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2025-02-11 12:05:52 -0800

    2181

---
+++ Leetcode/2181. Merge Nodes in Between Zeros/readme.md
@@ -0,0 +1,24 @@
+# Algorithm Idea
+
+Traverse the list with two pointers, where the second one moves till find a 0, meanwhile each non zero value is addem to a temporal counter. When it find a zero then the first pointers value become the counter, counter is restarted, second pointer becomes None or next if there is a next value and the first pointer next become the secn pointer while the first pointer moves to the current second. 
+
+# Complexity
+
+- Time: O(N)
+
+- Space:O(1)
+
+# Results
+
+Runtime
+
+128
+ms
+Beats
+77.84%
+
+Memory
+60.33
+MB
+Beats
+87.38%
---
+++ Leetcode/2181. Merge Nodes in Between Zeros/solution.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # use two pointers to find the two zeros, count the node values in between
+        # when the second zero is found make the value of the first zero the accumulated value
+        # and its next pointer to the second zero, the last zero is set to None
+        first_zero = head
+        second_zero = head
+        accum = 0
+
+        while second_zero:
+            if second_zero.val == 0 and second_zero != first_zero:
+                second_zero = second_zero.next if second_zero.next else None
+                first_zero.val = accum
+                first_zero.next = second_zero
+                first_zero = first_zero.next
+                accum = 0
+            else:
+                accum += second_zero.val
+                second_zero = second_zero.next
+
+        return head
