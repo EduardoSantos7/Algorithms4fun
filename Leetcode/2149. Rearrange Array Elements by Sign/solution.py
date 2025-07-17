9859cd38d38e0b1891333dfe7abdd0a9f02388a4
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2025-07-17 12:09:11 -0700

    2149 update

--- Leetcode/2149. Rearrange Array Elements by Sign/solution.py
+++ Leetcode/2149. Rearrange Array Elements by Sign/solution.py
@@ -1,24 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        arr1, arr2, result = [], [], []
 
-        # Initializing an answer array of size n
-        ans = [0] * n
-
-        # Initializing two pointers to track even and
-        # odd indices for positive and negative integers respectively
-        pos_index, neg_index = 0, 1
-
-        for i in range(n):
-            if nums[i] > 0:
-                # Placing the positive integer at the
-                # desired index in ans and incrementing pos_index by 2
-                ans[pos_index] = nums[i]
-                pos_index += 2
-            else:
-                # Placing the negative integer at the
-                # desired index in ans and incrementing neg_index by 2
-                ans[neg_index] = nums[i]
-                neg_index += 2
-
-        return ans
+        for num in nums:
+            target_arr = arr1 if num > 0 else arr2
+            target_arr.append(num)
+        
+        for a, b in zip(arr1, arr2):
+            result.append(a)
+            result.append(b)
+        
+        return result
